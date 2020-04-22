4b384b09761e54c92c75337183e5b3fbba0f8228
john <bblodfon@gmail.com>
2020-04-22 14:53:11 +0200

    add more tests for roc

--- tests/testthat/test-roc.R
+++ tests/testthat/test-roc.R
@@ -4,29 +4,53 @@ test_that("it returns proper results", {
   predictions = c(-3, -2, -1, 0, 1)
   observed    = c(1, 0, 1, 0, 0)
 
-  res = get_conf_mat_for_thres(predictions, observed, thres = -4)
+  res = get_conf_mat_for_thres(predictions, observed, thres = -4, direction = "<")
   expect_equal(res[["TP"]], 0)
   expect_equal(res[["FN"]], 2)
   expect_equal(res[["TN"]], 3)
   expect_equal(res[["FP"]], 0)
 
-  res = get_conf_mat_for_thres(predictions, observed, thres = -1.5)
+  res = get_conf_mat_for_thres(predictions, observed, thres = -4, direction = ">")
+  expect_equal(res[["TP"]], 2)
+  expect_equal(res[["FN"]], 0)
+  expect_equal(res[["TN"]], 0)
+  expect_equal(res[["FP"]], 3)
+
+  res = get_conf_mat_for_thres(predictions, observed, thres = -1.5, direction = "<")
   expect_equal(res[["TP"]], 1)
   expect_equal(res[["FN"]], 1)
   expect_equal(res[["TN"]], 2)
   expect_equal(res[["FP"]], 1)
 
-  res = get_conf_mat_for_thres(predictions, observed, thres = 0.5)
+  res = get_conf_mat_for_thres(predictions, observed, thres = -1.5, direction = ">")
+  expect_equal(res[["TP"]], 1)
+  expect_equal(res[["FN"]], 1)
+  expect_equal(res[["TN"]], 1)
+  expect_equal(res[["FP"]], 2)
+
+  res = get_conf_mat_for_thres(predictions, observed, thres = 0.5, direction = "<")
   expect_equal(res[["TP"]], 2)
   expect_equal(res[["FN"]], 0)
   expect_equal(res[["TN"]], 1)
   expect_equal(res[["FP"]], 2)
 
-  res = get_conf_mat_for_thres(predictions, observed, thres = 2)
+  res = get_conf_mat_for_thres(predictions, observed, thres = 0.5, direction = ">")
+  expect_equal(res[["TP"]], 0)
+  expect_equal(res[["FN"]], 2)
+  expect_equal(res[["TN"]], 2)
+  expect_equal(res[["FP"]], 1)
+
+  res = get_conf_mat_for_thres(predictions, observed, thres = 2, direction = "<")
   expect_equal(res[["TP"]], 2)
   expect_equal(res[["FN"]], 0)
   expect_equal(res[["TN"]], 0)
   expect_equal(res[["FP"]], 3)
+
+  res = get_conf_mat_for_thres(predictions, observed, thres = 2, direction = ">")
+  expect_equal(res[["TP"]], 0)
+  expect_equal(res[["FN"]], 2)
+  expect_equal(res[["TN"]], 3)
+  expect_equal(res[["FP"]], 0)
 })
 
 context("Testing 'get_roc_stats'")
@@ -36,7 +60,10 @@ test_that("it returns proper results", {
   observed    = c(1, 0, 1, 0, 0)
 
   roc_df = cbind.data.frame(predictions, observed)
-  res = get_roc_stats(df = roc_df, pred_col = "predictions", obs_col = "observed")
 
+  res = get_roc_stats(df = roc_df, pred_col = "predictions", label_col = "observed")
   expect_equal(res$AUC, 0.83333, tolerance = .00001)
+
+  res_rev = get_roc_stats(df = roc_df, pred_col = "predictions", label_col = "observed", direction = ">")
+  expect_equal(res_rev$AUC, 0.16666, tolerance = .00001)
 })
