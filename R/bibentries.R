06fc1c64c6a2f53d57aa75bf12df9135d901bc2e
john <bblodfon@gmail.com>
2023-09-16 21:09:43 +0200

    add file with bibtex entries

---
+++ R/bibentries.R
@@ -0,0 +1,13 @@
+bibentries = c(
+  davis2006 = bibentry("article",
+    title             = "The relationship between precision-recall and ROC curves",
+    author            = "J. Davis and M. Goadrich",
+    year              = 2006,
+    journal           = "Proceedings of the 23rd International Conference on Machine Learning",
+    publisher         = "ACM Press",
+    volume            = 148,
+    number            = 4,
+    pages             = "233--240",
+    url               = "https://doi.org/10.1145/1143844.1143874"
+  )
+)
